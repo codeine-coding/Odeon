48e66413c678fa914fd03a3c141f495d0a2914b3
Allen Whearry <allen@Buddha-Pro-15.local>
2019-05-11 23:29:34 -0400

    add ui test for getting screenshots

--- OdeonUITests/OdeonUITests.swift
+++ OdeonUITests/OdeonUITests.swift
@@ -9,6 +9,7 @@
 import XCTest
 
 class OdeonUITests: XCTestCase {
+    var app: XCUIApplication!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -17,7 +18,9 @@ class OdeonUITests: XCTestCase {
         continueAfterFailure = false
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -29,6 +32,20 @@ class OdeonUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+        let tabBarsQuery = app.tabBars
+        let categoriesButton = tabBarsQuery.buttons["Categories"]
+        categoriesButton.tap()
+        tabBarsQuery.buttons["QoTD"].tap()
+        snapshot("01QUOTD")
+        tabBarsQuery.buttons["Discover"].tap()
+        snapshot("O2Discover")
+        tabBarsQuery.buttons["Bookmarks"].tap()
+        snapshot("03Bookmarks")
+        categoriesButton.tap()
+        app.collectionViews/*@START_MENU_TOKEN@*/.staticTexts["Self Confidence"]/*[[".cells.staticTexts[\"Self Confidence\"]",".staticTexts[\"Self Confidence\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        snapshot("04CategorySelection")
+
     }
 
 }
