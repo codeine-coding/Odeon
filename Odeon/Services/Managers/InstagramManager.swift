1850c321f24fcba14fb28572a7bf422af75bb756
Allen Whearry <allen@Buddha-Pro-15.local>
2019-06-01 11:54:31 -0400

    delete duplicate files

--- Odeon/Services/Managers/InstagramManager.swift
+++ Odeon/Services/Managers/InstagramManager.swift
@@ -1,23 +1,21 @@
-//
-//  InstagramManager.swift
-//  TVQuoteApp
-//
-//  Created by Allen Whearry on 12/17/18.
-//  Copyright © 2018 Codeine Technologies LLC. All rights reserved.
-//
-
+////
+////  InstagramManager.swift
+////  InstagramSDK
+////
+////  Created by Attila Roy on 23/02/15.
+////  share image with caption to instagram
 import UIKit
 
 class InstagramManager: NSObject, UIDocumentInteractionControllerDelegate {
-    
+
     static let kInstagramURL = "instagram://app"
     static let kAlertViewTitle = "Error"
     static let kAlertViewMessage = "Please install the Instagram application"
     private let kUTI = "com.instagram.exclusivegram"
     private let kfileNameExtension = "instagram.igo"
-    
+
     var documentInteractionController = UIDocumentInteractionController()
-    
+
     // singleton manager
     class var sharedManager: InstagramManager {
         struct Singleton {
@@ -28,26 +26,25 @@ class InstagramManager: NSObject, UIDocumentInteractionControllerDelegate {
     
     func postImageToInstagramWithCaption(imageInstagram: UIImage, instagramCaption: String, view: UIView) {
         // called to post image with caption to the instagram application
-        
+
         let jpgPath = (NSTemporaryDirectory() as NSString).appendingPathComponent(kfileNameExtension)
         
         do {
             try imageInstagram.jpegData(compressionQuality: 1.0)?.write(to: URL(fileURLWithPath: jpgPath), options: .atomic)
-            
+
         } catch {
-            
+
             print(error)
         }
-        
+
         // let rect = CGRect(x: 0, y: 0, width: 1024, height: 1024)
         let fileURL = NSURL.fileURL(withPath: jpgPath)
         documentInteractionController.url = fileURL
         documentInteractionController.delegate = self
         documentInteractionController.uti = kUTI
-        
+
         // adding caption for the image
         documentInteractionController.annotation = ["InstagramCaption": instagramCaption]
         documentInteractionController.presentOpenInMenu(from: view.frame, in: view, animated: true)
     }
 }
-
